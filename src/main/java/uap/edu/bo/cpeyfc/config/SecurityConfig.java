ab40b033d5b0debab51d140bfec03e6664c5159d
LuchoNoPrograma <newusser2007@gmail.com>
2025-11-11 13:06:24 -0400

    Desarrollo: Corrección de textos en `LayoutCompleto.vue`, actualización de puerto en `vite.config.mjs` y adición de URL permitida en `SecurityConfig.java`

--- src/main/java/uap/edu/bo/cpeyfc/config/SecurityConfig.java
+++ src/main/java/uap/edu/bo/cpeyfc/config/SecurityConfig.java
@@ -104,6 +104,7 @@ public class SecurityConfig {
                 "http://localhost:5173",
                 "http://localhost:3000",
                 "http://127.0.0.1:5173",
+                "http://localhost:6063",
                 "https://*.trycloudflare.com"
         ));
 
